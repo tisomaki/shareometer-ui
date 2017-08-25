ab4acd49b1581e1f504330e9ea9d9a788c02fe0b
tisomaki <tisomaki@gmail.com>
2017-08-25 16:14:09 +0300

    another working set(not stable)

--- partials/app/shared/header/header.js
+++ partials/app/shared/header/header.js
@@ -1,20 +1,29 @@
 angular.
     module('header', []).
-    service(
-    'errorHandler', [MessageNotifier]).controller('errorController', ['errorHandler','$scope', function (errorHandler,$scope) {
+    factory(
+    'errorHandler', function() {
+        var obj = {};
+        obj.msgs=[];
+        obj.err = function(msg) {
+            obj.msgs.push({ type: "error", message: msg })
+            console.log("ErrorHandler.err: " + obj.msgs)
+        }
+        return obj;
+    }).controller('errorController', ['errorHandler','$scope', function (errorHandler,$scope) {
         console.log("constructor function for errorController")
         $scope.messages = errorHandler.msgs
-        console.log("-->"+$scope.messages)
+        console.log("setting scope array -->"+$scope.messages)
     }]);
 
 function MessageNotifier() {
-    var msgs = [];
+    this.msgs = [];
     this.err = function (msg) {
         
         this.msgs.push({ type: "error", message: msg })
         console.log("ErrorHandler.err: " + msgs)
         //$rootScope.$emit('notifying-service-event');
     }
+
 }
 console.log("end of header.js")
 //Genreate a watch alternative
